04a52f65f143c108eb1fa142a086d19ffa6b8199
anap7 <ana.flaviap7@hotmail.com>
2023-12-30 15:24:40 -0300

    update: change test

--- math_test.go
+++ math_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSome(t *testing.T) {
-	total := Soma(15, 15)
+	total := Soma(15, 13)
 
 	if total != 30 {
 		t.Errorf("Resultado da soma e invalido: Resultado %d. Resultado esperado: %d", total, 30)
